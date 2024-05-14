e64c7df5f76a877fd4033dc34fd5b2f6a9726100
biplavsitaula <biplav0007@gmail.com>
2024-05-14 22:49:28 +0545

    quotes changed

--- script.js
+++ script.js
@@ -3,34 +3,56 @@ const cssIcon = document.querySelector('.css');
 // import { dataArray } from './data'
 const codingQuotes = [
   "Code is like humor; when you have to explain it, it’s bad. - Cory House",
+
   "The most disastrous thing that you can ever learn is your first programming language. - Alan Kay",
+
   "Java is to JavaScript what car is to carpet. - Chris Heilmann",
+
   "It’s harder to read code than to write it. - Joel Spolsky",
+
   "Debugging is like trying to find a needle in a haystack, except the haystack is made of needles and you’re blindfolded. - Unknown",
+
   "First, solve the problem. Then, write the code. - John Johnson",
+
   "A programmer is a machine for turning coffee into code. - Unknown",
+
   "In software, the only numbers that matter are 0, 1, and N. - Unknown",
-  "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to produce bigger and better idiots. So far, the universe is winning. - Rick Cook",
+
   "To iterate is human, to recurse divine. - L. Peter Deutsch",
+
   "Perl – The only language that looks the same before and after RSA encryption. - Keith Bostic",
-  "Code generation, like drinking alcohol, is good in moderation. - Alex Lowe",
-  "Software and cathedrals are much the same – first we build them, then we pray. - Sam Redwine",
+
   "Simplicity is the soul of efficiency. - Austin Freeman",
+
   "Any fool can write code that a computer can understand. Good programmers write code that humans can understand. - Martin Fowler",
+
   "Don’t worry if it doesn’t work right. If everything did, you’d be out of a job. - Mosher’s Law of Software Engineering",
-  "Most good programmers do programming not because they expect to get paid or get adulation by the public, but because it is fun to program. - Linus Torvalds",
+
+
   "Talk is cheap. Show me the code. - Linus Torvalds",
+
   "The trouble with programmers is that you can never tell what a programmer is doing until it’s too late. - Seymour Cray",
+
   "Code never lies, comments sometimes do. - Ron Jeffries",
+
   "Premature optimization is the root of all evil. - Donald Knuth",
+
   "Programming is not about typing, it's about thinking. - Rich Hickey",
+
   "Programming is the closest thing we have to magic. - Drew Houston",
+
   "The best thing about a boolean is even if you are wrong, you are only off by a bit. - Unknown",
+
   "The only way to learn a new programming language is by writing programs in it. - Dennis Ritchie",
+
   "Every programmer is an author. - Sercan Leylek",
+
   "Code is like a poem; it has to follow certain structural requirements, but it should also be beautiful. - Unknown",
+
   "The computer was born to solve problems that did not exist before. - Bill Gates",
+
   "Software is a great combination between artistry and engineering. - Bill Gates",
+
   "The best software is written when the developers are sleeping. - Dave Thomas"
 ];
 
