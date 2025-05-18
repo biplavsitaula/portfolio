7b354a8ad1e84629f5494132505197e31f52ffef
biplavsitaula <biplav0007@gmail.com>
2025-05-19 00:45:55 +0545

    added new pages

--- script.js
+++ script.js
@@ -4,12 +4,23 @@ const theme = localStorage.getItem("theme");
 document.body.classList.toggle(theme);
 const toggleBtn = document.getElementById("theme-toggle");
 
+const url = window.location;
+
+if (url.pathname == "/") {
+  new Typewriter("#typewriter", {
+    strings: ["Developer", "Engineer"],
+    autoStart: true,
+    loop: true,
+  });
+}
+
 theme == "dark"
   ? (toggleBtn.style.backgroundImage = "url('./sun-solid.svg')")
   : (toggleBtn.style.backgroundImage = "url('./moon-solid.svg')");
 
-
 toggleBtn.addEventListener("click", () => {
+  console.log("clicking");
+
   document.body.classList.toggle("dark");
   document.body.classList.toggle("light");
 
@@ -17,7 +28,8 @@ toggleBtn.addEventListener("click", () => {
   const newTheme = document.body.classList.contains("dark") ? "dark" : "light";
   localStorage.setItem("theme", newTheme);
 
-  toggleBtn.style.transform = newTheme=='dark' ? "rotate(180deg)" : "rotate(0deg)";
+  toggleBtn.style.transform =
+    newTheme == "dark" ? "rotate(180deg)" : "rotate(0deg)";
   toggleBtn.style.backgroundImage =
     newTheme == "dark" ? "url('./sun-solid.svg')" : "url('./moon-solid.svg')";
 });
